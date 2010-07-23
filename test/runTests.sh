5eda40b37308d7bbf339cde01552bde36b424e1d
Rafal Przywarski <rp@rp-PC.(none)>
2010-07-24 00:14:34 +0200

    Acceptance test for Feature 1.2

--- test/runTests.sh
+++ test/runTests.sh
@@ -4,31 +4,65 @@ RASKC="$CURRENT_DIR/../bin/release/raskc"
 SUITE_LIST=`ls -d test*`
 OK_COUNT=0
 FAILED_COUNT=0
+ERROR_COUNT=0
 
 for SUITE in $SUITE_LIST
 do
-    echo "Running $SUITE..."
+    echo -n "$SUITE:"
     SUITE_DIR="$CURRENT_DIR/$SUITE"
     cd "$SUITE_DIR"
+
+    ERROR_LOG=""
+    
+    if [ ! -e "list" ]
+    then
+        echo "Missing $SUITE_DIR/list"
+        continue;
+    fi
+
     LIST=`cat list`
     for TEST in $LIST
     do
-        SOURCE="${TEST%:*}"
-        EXPECTED_OUTPUT=`cat ${TEST#*:}`
-        OUTPUT=`$RASKC $SOURCE 2>&1 1>/dev/null`
-        OUTPUT=${OUTPUT//$'\r'/""}
+        SOURCE="${TEST%%:*}"
+        EXPECTED_OUTPUT_PATH="${TEST##*:}"
+
+        if [ ! -e "$EXPECTED_OUTPUT_PATH" ]
+        then
+            echo "Missing $SUITE_DIR/$EXPECTED_OUTPUT_PATH"
+            ((ERROR_COUNT+=1))
+            continue;
+        fi
+
+        EXPECTED_OUTPUT=`cat $EXPECTED_OUTPUT_PATH`
+        TEST_TYPE=${TEST%:*}
+        TEST_TYPE=${TEST_TYPE#*:}
+        case "$TEST_TYPE" in
+            "error") 
+                OUTPUT=`$RASKC $SOURCE -n 2>&1 1>/dev/null`;;
+            "test")
+                $RASKC "$SOURCE" -o prog.out 2>/dev/null 1>/dev/null;
+                OUTPUT=`./prog.out 2>/dev/null`;
+                rm -f prog.out;;
+            *)
+                ((ERROR_COUNT+=1));
+                echo -n "E";
+                continue;;
+        esac
+	
+        OUTPUT=${OUTPUT//$'\r'/""};
+
         if [ "$OUTPUT" == "$EXPECTED_OUTPUT" ]
         then
             ((OK_COUNT+=1))
+            echo -n "."
         else
-            echo "FAILED $TEST"
-            echo "expected:"
-            echo "$EXPECTED_OUTPUT"
-            echo "got:"
-            echo "$OUTPUT"
+            ERROR_LOG="${ERROR_LOG}FAILED $TEST\nexpected:\n$EXPECTED_OUTPUT\ngot:\n$OUTPUT\n"
             ((FAILED_COUNT+=1))
+            echo -n "F"
         fi
     done
+    echo
+    echo -ne "$ERROR_LOG"
 done
 if [ "$OK_COUNT" -eq 0 ]
 then
@@ -42,6 +76,12 @@ then
 else
     FAILED_MSG=" failed:$FAILED_COUNT"
 fi
+if [ "$ERROR_COUNT" -eq 0 ]
+then
+    ERROR_MSG=""
+else
+    ERROR_MSG=" errors:$ERROR_COUNT"
+fi
 echo
-echo "Summary:$OK_MSG$FAILED_MSG"
+echo "Summary:$OK_MSG$FAILED_MSG$ERROR_MSG"
 
