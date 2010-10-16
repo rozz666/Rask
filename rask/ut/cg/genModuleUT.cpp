4c6c76f672472b2e49e18eb963f9f1b7086735e5
Rafal Przywarski <rp@myhost.localdomain>
2010-10-16 10:08:08 +0200

    Refactored genModule UTs

--- rask/ut/cg/genModuleUT.cpp
+++ rask/ut/cg/genModuleUT.cpp
@@ -12,6 +12,7 @@
 #include <llvm/DerivedTypes.h>
 #include <rask/test/TUTAssert.hpp>
 #include <rask/cg/CodeGenerator.hpp>
+#include <rask/test/Mock.hpp>
 
 namespace tut
 {
@@ -28,57 +29,17 @@ namespace
 {
 tut::factory tf("rask.cg.CodeGenerator.genModule");
 
-class CodeGeneratorMock : public rask::cg::CodeGenerator
+MOCK(CodeGeneratorMock, rask::cg::CodeGenerator)
 {
 public:
 
-    struct GenFunction
-    {
-        int N;
-        const rask::ast::CustomFunction *f;
-        llvm::Module *module;
-    };
-    
-    struct DeclFunction
-    {
-        int N;
-        const rask::ast::CustomFunction *f;
-        llvm::Module *module;
-    };
-    
-    int counter;
-    int declBuiltinFunctionsCalled;
-    llvm::Module *declBuiltinFunctionsModule;
-    std::vector<DeclFunction> declFunctionCalls;
-    std::vector<GenFunction> genFunctionCalls;
-    
-    CodeGeneratorMock()
-        : rask::cg::CodeGenerator(dummyST_), counter(0), 
-        declBuiltinFunctionsCalled(0), declBuiltinFunctionsModule(0) { }
-    
-    virtual void genFunction(const rask::ast::CustomFunction& f, llvm::Module& module)
-    {
-        GenFunction gf = { ++counter, &f, &module };
-
-        genFunctionCalls.push_back(gf);
-    }
-
-    virtual void declFunction(const rask::ast::CustomFunction& f, llvm::Module& module)
-    {
-        DeclFunction df = { ++counter, &f, &module };
-        
-        declFunctionCalls.push_back(df);
-    }
-
-    virtual void declBuiltinFunctions(llvm::Module& module)
-    {
-        declBuiltinFunctionsCalled = ++counter;
-        declBuiltinFunctionsModule = &module;
-    }
-    
-private:
-        
     rask::cg::SymbolTable dummyST_;
+    
+    CodeGeneratorMock() : rask::cg::CodeGenerator(dummyST_) { }
+    
+    MOCK_METHOD(void, genFunction, (const rask::ast::CustomFunction&, f)(llvm::Module&, module));
+    MOCK_METHOD(void, declFunction, (const rask::ast::CustomFunction&, f)(llvm::Module&, module));
+    MOCK_METHOD(void, declBuiltinFunctions, (llvm::Module&, module));
 };
 
 }
@@ -93,9 +54,9 @@ void object::test<1>()
     using namespace rask;
     
     llvm::LLVMContext context;
-    ast::Tree ast;
     ast::SharedCustomFunction f1(new ast::CustomFunction(cst::Identifier::create(Position(), "abc")));
     ast::SharedCustomFunction f2(new ast::CustomFunction(cst::Identifier::create(Position(), "def")));
+    ast::Tree ast;
     ast.add(f1);
     ast.add(f2);
     
@@ -105,23 +66,11 @@ void object::test<1>()
 
     ensure_equals("context", &module->getContext(), &context);
 
-    ensure_equals("builtin", cg.declBuiltinFunctionsCalled, 1);
-
-    ensure_equals("2 decls", cg.declFunctionCalls.size(), 2u);
-    ensure("decl 1 N", cg.declFunctionCalls[0].N == 2);
-    ensure("decl 1 f", cg.declFunctionCalls[0].f == f1.get());
-    ensure("decl 1 m", cg.declFunctionCalls[0].module == module.get());
-    ensure("decl 2 N", cg.declFunctionCalls[1].N == 3);
-    ensure("decl 2 f", cg.declFunctionCalls[1].f == f2.get());
-    ensure("decl 2 m", cg.declFunctionCalls[1].module == module.get());
-    
-    ensure_equals("2 gens", cg.genFunctionCalls.size(), 2u);
-    ensure("gen 1 N", cg.genFunctionCalls[0].N == 4);
-    ensure("gen 1 f", cg.genFunctionCalls[0].f == f1.get());
-    ensure("gen 1 m", cg.genFunctionCalls[0].module == module.get());
-    ensure("gen 2 N", cg.genFunctionCalls[1].N == 5);
-    ensure("gen 2 f", cg.genFunctionCalls[1].f == f2.get());
-    ensure("gen 2 m", cg.genFunctionCalls[1].module == module.get());
+    ENSURE_CALL(cg, declBuiltinFunctions(*module));
+    ENSURE_CALL(cg, declFunction(*f1, *module));
+    ENSURE_CALL(cg, declFunction(*f2, *module));
+    ENSURE_CALL(cg, genFunction(*f1, *module));
+    ENSURE_CALL(cg, genFunction(*f2, *module));
 }
 
 
