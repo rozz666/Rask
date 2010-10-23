e44051ffec1ef748c526a0b33c83d814a065c3b3
Rafal Przywarski <rp@myhost.localdomain>
2010-10-23 14:12:06 +0200

    Extracted CodeGenerator::genValue

--- rask/cg/genFunctionCall.cpp
+++ rask/cg/genFunctionCall.cpp
@@ -16,42 +16,16 @@ namespace rask
 {
 namespace cg
 {
-namespace
-{
-    
-struct GetValue : boost::static_visitor<llvm::Value *>
-{
-    llvm::BasicBlock& block;
-    llvm::Module& module;
-    SymbolTable& symbolTable;
-
-    GetValue(llvm::BasicBlock& block, llvm::Module& module, SymbolTable& symbolTable)
-        : block(block), module(module), symbolTable(symbolTable) { }
-    
-    llvm::Value *operator()(const ast::Constant& c)
-    {
-        return llvm::ConstantInt::get(module.getContext(), llvm::APInt(32, c, true));
-    }
-
-    llvm::Value *operator()(const ast::WeakVariable& var)
-    {
-        return new llvm::LoadInst(symbolTable.get(var.lock()->name()), "", &block);
-    }
-};
-
-}
     
 llvm::CallInst *CodeGenerator::genFunctionCall(const ast::FunctionCall& fc, llvm::BasicBlock& block, llvm::Module& module)
 {
-    GetValue getValue(block, module, symbolTable_);
-
     ast::SharedFunction f = fc.function().lock();
 
     std::vector<llvm::Value *> args;
     
     BOOST_FOREACH(const ast::Expression& e, fc.args())
     {
-        args.push_back(e.apply_visitor(getValue));
+        args.push_back(genValue(e, symbolTable_, block, module));
     }
     
     return llvm::CallInst::Create(module.getFunction(f->name().value), args.begin(), args.end(), "", &block);
---
+++ rask/cg/genValue.cpp
@@ -0,0 +1,47 @@
+// Rask
+//
+// Copyright (c) 2010 Rafal Przywarski
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+#include <rask/cg/CodeGenerator.hpp>
+
+namespace rask
+{
+namespace cg
+{
+namespace
+{
+
+struct GetValue : boost::static_visitor<llvm::Value *>
+{
+    llvm::BasicBlock& block;
+    llvm::Module& module;
+    const SymbolTable& symbolTable;
+
+    GetValue(llvm::BasicBlock& block, llvm::Module& module, const SymbolTable& symbolTable)
+        : block(block), module(module), symbolTable(symbolTable) { }
+
+    llvm::Value *operator()(const ast::Constant& c)
+    {
+        return llvm::ConstantInt::get(module.getContext(), llvm::APInt(32, c, true));
+    }
+
+    llvm::Value *operator()(const ast::WeakVariable& var)
+    {
+        return new llvm::LoadInst(symbolTable.get(var.lock()->name()), "", &block);
+    }
+};
+
+}
+    
+llvm::Value *CodeGenerator::genValue(const ast::Expression& expr, const SymbolTable& symbolTable, llvm::BasicBlock& block, llvm::Module& module)
+{
+    GetValue getValue(block, module, symbolTable);
+    return expr.apply_visitor(getValue);
+}
+    
+}
+}
