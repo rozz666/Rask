acca7cfa7b85e2802bd8283fb90d0125394a5507
Rafal Przywarski <rp@myhost.localdomain>
2011-01-06 17:27:52 +0100

    Removed unused SymbolTable functionality

--- rask/ast/SymbolTable.hpp
+++ rask/ast/SymbolTable.hpp
@@ -25,65 +25,25 @@ class SymbolTable
 {
 public:
 
-    SymbolTable() : vars_(1) { }
-
-
-    SharedVariable add(SharedVariable var)
-    {
-        return add(vars_.back(), var);
-    }
-
     SharedFunction add(SharedFunction function)
     {
-        return add(functions_, function);
-    }
-
-    boost::optional<SharedVariable> getVariable(const std::string& name) const
-    {
-        return get(vars_.back(), name);
+        return functions_.insert(Functions::value_type(function->name().value, function)).first->second;
     }
 
     boost::optional<SharedFunction> getFunction(const std::string& name) const
     {
-        return get(functions_, name);
-    }
-
-    void enterScope()
-    {
-        vars_.push_back(Vars::value_type());
-    }
-
-    void exitScope()
-    {
-        if (vars_.size() == 1) throw std::out_of_range("SymbolTable exit scope");
-
-        vars_.pop_back();
-    }
-
-private:
-
-    typedef std::vector<std::map<std::string, SharedVariable> > Vars;
-    typedef std::map<std::string, SharedFunction> Functions;
-
-    Vars vars_;
-    Functions functions_;
-
-    template <typename M>
-    static typename M::mapped_type add(M& m, const typename M::mapped_type& v)
-    {
-        return m.insert(typename M::value_type(v->name().value, v)).first->second;
-    }
-
-    template <typename M>
-    static boost::optional<typename M::mapped_type> get(const M& m, const std::string& name)
-    {
-        typename M::const_iterator it = m.find(name);
+        typename Functions::const_iterator it = functions_.find(name);
         
-        if (it == m.end()) return boost::none;
+        if (it == functions_.end()) return boost::none;
         
         return it->second;
     }
 
+private:
+
+    typedef std::map<std::string, SharedFunction> Functions;
+
+    Functions functions_;
 };
 
 }
--- rask/ut/ast/SymbolTableUT.cpp
+++ rask/ut/ast/SymbolTableUT.cpp
@@ -11,7 +11,6 @@
 #include <rask/test/TUTAssert.hpp>
 #include <rask/ast/SymbolTable.hpp>
 #include <rask/test/FunctionFactory.hpp>
-#include <rask/test/VariableFactory.hpp>
 
 namespace tut
 {
@@ -20,12 +19,6 @@ struct AstSymbolTable_TestData
 {
     rask::ast::SymbolTable st;
     rask::test::FunctionFactory functionFactory;
-    rask::test::VariableFactory variableFactory;
-    std::string var1Name;
-    std::string var2Name;
-    rask::ast::SharedVariable var1a;
-    rask::ast::SharedVariable var1b;
-    rask::ast::SharedVariable var2;
     std::string f1Name;
     std::string f2Name;
     rask::ast::SharedFunction f1a;
@@ -33,11 +26,7 @@ struct AstSymbolTable_TestData
     rask::ast::SharedFunction f2;
 
     AstSymbolTable_TestData()
-        : var1Name("asia"), var2Name("kasia"),
-        var1a(variableFactory.createShared(var1Name)),
-        var1b(variableFactory.createShared(var1Name)),
-        var2(variableFactory.createShared(var2Name)),
-        f1Name("asia"), f2Name("kasia"),
+        : f1Name("asia"), f2Name("kasia"),
         f1a(functionFactory.createShared(f1Name)),
         f1b(functionFactory.createShared(f1Name)),
         f2(functionFactory.createShared(f2Name))
@@ -61,23 +50,6 @@ template <>
 template <>
 void object::test<1>()
 {
-    ENSURE(st.add(var1a) == var1a);
-    ENSURE(st.add(var2) == var2);
-    ENSURE(*st.getVariable(var1Name) == var1a);
-    ENSURE(*st.getVariable(var2Name) == var2);
-}
-
-template <>
-template <>
-void object::test<2>()
-{
-    ENSURE(!st.getVariable("x"));
-}
-
-template <>
-template <>
-void object::test<3>()
-{
     ENSURE(st.add(f1a) == f1a);
     ENSURE(st.add(f2) == f2);
     ENSURE(*st.getFunction(f1Name) == f1a);
@@ -86,73 +58,18 @@ void object::test<3>()
 
 template <>
 template <>
-void object::test<4>()
+void object::test<2>()
 {
     ENSURE(!st.getFunction("x"));
 }
 
 template <>
 template <>
-void object::test<5>()
-{
-    ENSURE(st.add(var1a) == var1a);
-    ENSURE(st.add(var1b) == var1a);
-    ENSURE(*st.getVariable(var1Name) == var1a);
-}
-
-template <>
-template <>
-void object::test<6>()
+void object::test<3>()
 {
     ENSURE(st.add(f1a) == f1a);
     ENSURE(st.add(f1b) == f1a);
     ENSURE(st.getFunction(f1Name) == f1a);
 }
-
-template <>
-template <>
-void object::test<7>()
-{
-    st.add(var1a);
-    st.enterScope();
-
-    ENSURE(st.add(var1b) == var1b);
-    ENSURE(*st.getVariable(var1Name) == var1b);
-}
-
-template <>
-template <>
-void object::test<8>()
-{
-    st.add(var1a);
-    st.enterScope();
-    st.add(var1b);
-    st.exitScope();
-
-    ENSURE(*st.getVariable(var1Name) == var1a);
-}
-
-template <>
-template <>
-void object::test<9>()
-{
-    st.enterScope();
-    st.add(var1a);
-    st.exitScope();
-    st.enterScope();
-
-    ENSURE(!st.getVariable(var1Name));
-}
-
-template <>
-template <>
-void object::test<10>()
-{
-    ENSURE_THROWS(st.exitScope(), std::out_of_range);
-    st.enterScope();
-    st.exitScope();
-    ENSURE_THROWS(st.exitScope(), std::out_of_range);
-}
-
 
 }
