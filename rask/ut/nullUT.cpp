26fc739e4c2e56e56d9e0031ab99ca7f6f29c0d7
Rafal Przywarski <rp@myhost.localdomain>
2011-01-13 23:10:12 +0100

    Ported null UTs to GTest

--- rask/ut/nullUT.cpp
+++ rask/ut/nullUT.cpp
@@ -6,65 +6,38 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
-#include <tut/tut.hpp>
-#include <rask/test/TUTAssert.hpp>
+#include <gtest/gtest.h>
 #include <rask/null.hpp>
-#include <boost/concept_check.hpp>
 
-namespace tut
-{
-
-struct null_TestData
-{
-};
-
-typedef test_group<null_TestData> factory;
-typedef factory::object object;
-}
-
-namespace
-{
-tut::factory tf("rask.null");
-}
-
-namespace tut
-{
-
-template <>
-template <>
-void object::test<1>()
+TEST(rask_null, pointerInitialization)
 {
     void *v = rask::null;
     char *c = rask::null;
     boost::shared_ptr<float> s = rask::null;
     boost::weak_ptr<double> w = rask::null;
 
-    ENSURE(v == 0);
-    ENSURE(c == 0);
-    ENSURE(s == boost::shared_ptr<float>());
-    ENSURE(w.expired());
+    ASSERT_TRUE(v == 0);
+    ASSERT_TRUE(c == 0);
+    ASSERT_TRUE(s == boost::shared_ptr<float>());
+    ASSERT_TRUE(w.expired());
 }
 
-template <>
-template <>
-void object::test<2>()
+TEST(rask_null, pointerEquality)
 {
     int *p1 = 0;
     int x;
     int *p2 = &x;
 
-    ENSURE(rask::null == p1);
-    ENSURE(p1 == rask::null);
-    ENSURE(!(rask::null != p1));
-    ENSURE(!(p1 != rask::null));
+    ASSERT_TRUE(rask::null == p1);
+    ASSERT_TRUE(p1 == rask::null);
+    ASSERT_TRUE(!(rask::null != p1));
+    ASSERT_TRUE(!(p1 != rask::null));
 
-    ENSURE(rask::null != p2);
-    ENSURE(p2 != rask::null);
-    ENSURE(!(rask::null == p2));
-    ENSURE(!(p2 == rask::null));
+    ASSERT_TRUE(rask::null != p2);
+    ASSERT_TRUE(p2 != rask::null);
+    ASSERT_TRUE(!(rask::null == p2));
+    ASSERT_TRUE(!(p2 == rask::null));
 
-    ENSURE(rask::null == rask::null);
-    ENSURE(!(rask::null != rask::null));
-}
-
+    ASSERT_TRUE(rask::null == rask::null);
+    ASSERT_TRUE(!(rask::null != rask::null));
 }
