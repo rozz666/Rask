71224b3783fc1342b1d59228eedad8b8404f0d67
Rafal Przywarski <rp@myhost.localdomain>
2011-01-14 00:16:39 +0100

    Ported ast.Constant UTs to GTest and refactored

--- rask/ut/ast/ConstantUT.cpp
+++ rask/ut/ast/ConstantUT.cpp
@@ -6,108 +6,69 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
-#include <tut/tut.hpp>
-#include <rask/test/TUTAssert.hpp>
 #include <rask/ast/Constant.hpp>
+#include <gtest/gtest.h>
 
-namespace tut
+using namespace rask::ast;
+
+TEST(rask_ast_Constant, int32Equality)
 {
-
-struct Constant_TestData
-{
-};
-
-typedef test_group<Constant_TestData> factory;
-typedef factory::object object;
-}
-
-namespace
-{
-tut::factory tf("rask.ast.Constant");
-}
-
-namespace tut
-{
-
-template <>
-template <>
-void object::test<1>()
-{
-    using namespace rask;
     boost::int32_t n = 35;
-    ast::Constant c1(boost::int32_t(77));
-    ast::Constant c2(n);
-    ast::Constant c3(n);
+    Constant c1(boost::int32_t(77));
+    Constant c2(n);
+    Constant c3(n);
 
-    ENSURE(c1 != c2);
-    ENSURE(!(c1 == c2));
-    ENSURE(c2 == c3);
-    ENSURE(!(c2 != c3));
+    ASSERT_TRUE(c1 != c2);
+    ASSERT_FALSE(c1 == c2);
+    ASSERT_TRUE(c2 == c3);
+    ASSERT_FALSE(c2 != c3);
 }
 
-template <>
-template <>
-void object::test<2>()
+TEST(rask_ast_Constant, getInt32)
 {
-    using namespace rask;
     boost::int32_t n1 = 77;
     boost::int32_t n2 = 35;
-    ast::Constant c1(n1);
-    ast::Constant c2(n2);
+    Constant c1(n1);
+    Constant c2(n2);
 
-    ENSURE_EQUALS(c1.getInt32(), n1);
-    ENSURE_EQUALS(c2.getInt32(), n2);
+    ASSERT_EQ(n1, c1.getInt32());
+    ASSERT_EQ(n2, c2.getInt32());
 }
 
-template <>
-template <>
-void object::test<3>()
+TEST(rask_ast_Constant, booleanEquality)
 {
-    using namespace rask;
-    ast::Constant c1(true);
-    ast::Constant c2(false);
-    ast::Constant c3(false);
+    Constant c1(true);
+    Constant c2(false);
+    Constant c3(false);
 
-    ENSURE(c1 != c2);
-    ENSURE(!(c1 == c2));
-    ENSURE(c2 == c3);
-    ENSURE(!(c2 != c3));
+    ASSERT_TRUE(c1 != c2);
+    ASSERT_FALSE(c1 == c2);
+    ASSERT_TRUE(c2 == c3);
+    ASSERT_FALSE(c2 != c3);
 }
 
-template <>
-template <>
-void object::test<4>()
+TEST(rask_ast_Constant, getBoolean)
 {
-    using namespace rask;
     bool v1 = true;
     bool v2 = false;
-    ast::Constant c1(v1);
-    ast::Constant c2(v2);
+    Constant c1(v1);
+    Constant c2(v2);
 
-    ENSURE(c1.getBoolean() == v1);
-    ENSURE(c2.getBoolean() == v2);
+    ASSERT_EQ(v1, c1.getBoolean());
+    ASSERT_EQ(v2, c2.getBoolean());
 }
 
-template <>
-template <>
-void object::test<5>()
+TEST(rask_ast_Constant, mixedEquality)
 {
-    using namespace rask;
-
-    ENSURE(ast::Constant(false) != ast::Constant(boost::int32_t(0)));
-    ENSURE(!(ast::Constant(false) == ast::Constant(boost::int32_t(0))));
-    ENSURE(ast::Constant(boost::int32_t(0)) != ast::Constant(false));
-    ENSURE(!(ast::Constant(boost::int32_t(0)) == ast::Constant(false)));
+    ASSERT_TRUE(Constant(false) != Constant(boost::int32_t(0)));
+    ASSERT_FALSE(Constant(false) == Constant(boost::int32_t(0)));
+    ASSERT_TRUE(Constant(boost::int32_t(0)) != Constant(false));
+    ASSERT_FALSE(Constant(boost::int32_t(0)) == Constant(false));
 }
 
-template <>
-template <>
-void object::test<6>()
+TEST(rask_ast_Constant, typeQuery)
 {
-    using namespace rask;
-
-    ENSURE(ast::Constant(boost::int32_t(11)).type() == ast::INT32);
-    ENSURE(ast::Constant(true).type() == ast::BOOLEAN);
+    ASSERT_TRUE(Constant(boost::int32_t(11)).type() == INT32);
+    ASSERT_TRUE(Constant(true).type() == BOOLEAN);
 }
 
-}
