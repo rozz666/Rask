a17beb4434e519b3b1f9e565447d190202fc1485
Rafal Przywarski <rp@myhost.localdomain>
2011-01-13 21:09:38 +0100

    Ported cst.InputStream UTs to GTest

--- rask/ut/InputStreamUT.cpp
+++ rask/ut/InputStreamUT.cpp
@@ -6,41 +6,18 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
+#include <gtest/gtest.h>
 #include <sstream>
 #include <rask/InputStream.hpp>
-#include <tut/tut.hpp>
-#include <tut/../contrib/tut_macros.h>
 
-namespace tut
-{
-
-struct InputStream_TestData
-{
-};
-
-typedef test_group<InputStream_TestData> factory;
-typedef factory::object object;
-}
-
-namespace
-{
-tut::factory tf("rask.InputStream");
-}
-
-namespace tut
-{
-
-template <>
-template <>
-void object::test<1>()
+TEST(rask_InputStream, iterators)
 {
     std::string text = "Asia abc";
+    std::string file = "xxx";
     std::stringstream ss(text);
-    rask::InputStream is("xxx", ss);
+    rask::InputStream is(file, ss);
 
-    ensure_equals("file", is.file(), "xxx");
-    ensure_equals("source", std::string(is.begin(), is.end()), text);
+    ASSERT_EQ(file, is.file());
+    ASSERT_EQ(text, std::string(is.begin(), is.end()));
 }
 
-
-}
