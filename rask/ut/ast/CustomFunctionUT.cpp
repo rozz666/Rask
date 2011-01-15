4af121e1565e5bfcc222e1baf5da7abd66906927
Rafal Przywarski <rp@myhost.localdomain>
2011-01-15 22:26:37 +0100

    Ported ast.CustomFunction UTs to GTest

--- rask/ut/ast/CustomFunctionUT.cpp
+++ rask/ut/ast/CustomFunctionUT.cpp
@@ -6,64 +6,43 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
-#include <tut/tut.hpp>
-#include <tut/../contrib/tut_macros.h>
-#include <boost/assign/list_of.hpp>
-#include <rask/test/TUTAssert.hpp>
 #include <rask/ast/CustomFunction.hpp>
+#include <boost/assign/list_of.hpp>
 #include <rask/test/VariableDeclFactory.hpp>
 #include <rask/test/VariableFactory.hpp>
+#include <gtest/gtest.h>
 
-namespace tut
-{
+using namespace rask;
+using namespace testing;
 
-struct CustomFunction_TestData
+struct rask_ast_CustomFunction : testing::Test
 {
-    rask::test::VariableDeclFactory varDeclFactory;
+    test::VariableDeclFactory varDeclFactory;
 };
 
-typedef test_group<CustomFunction_TestData> factory;
-typedef factory::object object;
-}
-
-namespace
+TEST_F(rask_ast_CustomFunction, twoStatements)
 {
-tut::factory tf("rask.ast.CustomFunction");
-}
-
-namespace tut
-{
-
-template <>
-template <>
-void object::test<1>()
-{
-    using namespace rask;
-
     cst::Identifier name = cst::Identifier::create(Position("abc", 10, 20), "kasia");
     ast::CustomFunction cf(name, ast::VOID);
     ast::Function& f = cf;
     ast::SharedCustomFunction dummy(new ast::CustomFunction(cst::Identifier::create(Position(), "test"), ast::VOID));
     ast::FunctionCall fc(dummy, ast::FunctionCall::Arguments());
     ast::VariableDecl vd = varDeclFactory.create("asia");
-    ENSURE_EQUALS(cf.stmtCount(), 0u);
+    ASSERT_EQ(0u, cf.stmtCount());
     cf.addStmt(fc);
     cf.addStmt(vd);
-    ENSURE_EQUALS(f.name().position, name.position);
-    ENSURE_EQUALS(f.name().value, name.value);
-    ENSURE_EQUALS(f.argCount(), 0u);
-    ENSURE_EQUALS(f.argTypes().size(), 0u);
-    ENSURE_EQUALS(cf.stmtCount(), 2u);
-    ENSURE(getFunctionCall(cf.stmt(0)).function().lock() == dummy);
-    ENSURE_EQUALS(getFunctionCall(cf.stmt(0)).args().size(), 0u);
-    ENSURE(getVariableDecl(cf.stmt(1)).var() == vd.var());
+    ASSERT_EQ(name.position, f.name().position);
+    ASSERT_EQ(name.value, f.name().value);
+    ASSERT_EQ(0u, f.argCount());
+    ASSERT_EQ(0u, f.argTypes().size());
+    ASSERT_EQ(2u, cf.stmtCount());
+    ASSERT_TRUE(getFunctionCall(cf.stmt(0)).function().lock() == dummy);
+    ASSERT_EQ(0u, getFunctionCall(cf.stmt(0)).args().size());
+    ASSERT_TRUE(getVariableDecl(cf.stmt(1)).var() == vd.var());
 }
 
-template <>
-template <>
-void object::test<2>()
+TEST_F(rask_ast_CustomFunction, arguments)
 {
-    using namespace rask;
     using boost::assign::list_of;
 
     ast::CustomFunction cf(cst::Identifier::create(Position(), "xxx"), ast::VOID);
@@ -72,26 +51,21 @@ void object::test<2>()
     ast::SharedVariable v2 = variableFactory.createShared("b", ast::BOOLEAN);
 
     cf.addArg(v1);
-    ENSURE(cf.argTypes() == list_of(ast::INT32));
-    ENSURE_EQUALS(cf.argCount(), 1u);
-    ENSURE(cf.arg(0) ==  v1);
+    ASSERT_TRUE(cf.argTypes() == list_of(ast::INT32));
+    ASSERT_EQ(1u, cf.argCount());
+    ASSERT_TRUE(cf.arg(0) ==  v1);
     cf.addArg(v2);
-    ENSURE(cf.argTypes() == list_of(ast::INT32)(ast::BOOLEAN));
-    ENSURE_EQUALS(cf.argCount(), 2u);
-    ENSURE(cf.arg(1) ==  v2);
+    ASSERT_TRUE(cf.argTypes() == list_of(ast::INT32)(ast::BOOLEAN));
+    ASSERT_EQ(2u, cf.argCount());
+    ASSERT_TRUE(cf.arg(1) ==  v2);
 }
 
-template <>
-template <>
-void object::test<3>()
+TEST_F(rask_ast_CustomFunction, type)
 {
-    using namespace rask;
-
     ast::CustomFunction cf1(cst::Identifier::create(Position(), "abc"), ast::VOID);
     ast::CustomFunction cf2(cst::Identifier::create(Position(), "abc"), ast::INT32);
 
-    ENSURE(cf1.type() == ast::VOID);
-    ENSURE(cf2.type() == ast::INT32);
+    ASSERT_TRUE(cf1.type() == ast::VOID);
+    ASSERT_TRUE(cf2.type() == ast::INT32);
 }
 
-}
