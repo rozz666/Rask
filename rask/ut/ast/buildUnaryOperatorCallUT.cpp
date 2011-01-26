8d7e1d995958d9da2de50be4f0569f6450434962
Rafal Przywarski <rp@myhost.localdomain>
2011-01-26 23:39:34 +0100

    Ported ast.Builder.buildUnaryOperatorCall UTs to GTest

--- rask/ut/ast/buildUnaryOperatorCallUT.cpp
+++ rask/ut/ast/buildUnaryOperatorCallUT.cpp
@@ -6,78 +6,56 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
-#include <tut/tut.hpp>
-#include <rask/test/TUTAssert.hpp>
-#include <rask/test/Mock.hpp>
 #include <rask/ast/Builder.hpp>
 #include <rask/test/FunctionFactory.hpp>
 #include <rask/Operators.hpp>
-#include <rask/ut/ast/ScopeMock.hpp>
+#include <rask/ut/ast/ScopeMockNew.hpp>
+#include <rask/ut/cst/OperatorStubs.hpp>
 #include <rask/null.hpp>
+#include <gtest/gtest.h>
 
 using namespace rask;
+using namespace testing;
 
 namespace
 {
 
-CLASS_MOCK(BuilderMock, rask::ast::Builder)
+struct BuilderMock : ast::Builder
 {
-    BuilderMock(ast::SharedFunctionTable ft) : rask::ast::Builder(null, ft, null) { }
+    BuilderMock(ast::SharedFunctionTable ft) : ast::Builder(null, ft, null) { }
 
-    MOCK_METHOD(boost::optional<rask::ast::Expression>, buildExpression,
-        (const rask::cst::Expression&, expr)(rask::ast::SharedScope, scope))
+    MOCK_METHOD2(buildExpression, boost::optional<ast::Expression>(const cst::Expression&, ast::SharedScope));
 };
 
 }
 
-namespace tut
-{
-
-struct buildUnaryOperatorCall_TestData
+struct rask_ast_Builder_buildUnaryOperatorCall : testing::Test
 {
     ast::SharedFunctionTable ft;
     BuilderMock builder;
-    rask::ast::test::SharedScopeMock scope;
+    ast::SharedScope scope;
 
-    buildUnaryOperatorCall_TestData()
-        : ft(new ast::FunctionTable), builder(ft), scope(new rask::ast::test::ScopeMock) { }
+    rask_ast_Builder_buildUnaryOperatorCall()
+        : ft(new ast::FunctionTable), builder(ft), scope(new ast::ScopeMock) { }
 };
 
-typedef test_group<buildUnaryOperatorCall_TestData> factory;
-typedef factory::object object;
-}
-
-namespace
+TEST_F(rask_ast_Builder_buildUnaryOperatorCall, minus)
 {
-tut::factory tf("rask.ast.Builder.buildUnaryOperatorCall");
-}
-
-namespace tut
-{
-
-template <>
-template <>
-void object::test<1>()
-{
-    using namespace rask;
-
     cst::UnaryOperatorCall oc;
     oc.op.tag = cst::UnaryOperator::MINUS;
 
-    test::FunctionFactory functionFactory;
-    ast::SharedCustomFunction f = functionFactory.createShared(UNARY_MINUS_NAME, ast::INT32, 1);
+    ast::SharedCustomFunction f = test::FunctionFactory().createShared(UNARY_MINUS_NAME, ast::INT32, 1);
     ast::Constant retExpr(7);
-    MOCK_RETURN(builder, buildExpression, ast::Expression(retExpr));
+    EXPECT_CALL(builder, buildExpression(Ref(oc.expr), scope))
+        .WillOnce(Return(ast::Expression(retExpr)));
 
     ft->add(f);
 
     boost::optional<ast::FunctionCall> fc = builder.buildUnaryOperatorCall(oc, scope);
 
-    ENSURE(fc);
-    ENSURE(fc->function().lock() == f);
-    ENSURE_EQUALS(fc->args().size(), 1u);
-    ENSURE(getConstant(fc->args()[0]) == retExpr);
-    ENSURE_CALL(builder, buildExpression(oc.expr, scope));
+    ASSERT_TRUE(fc);
+    ASSERT_TRUE(fc->function().lock() == f);
+    ASSERT_EQ(1u, fc->args().size());
+    ASSERT_TRUE(getConstant(fc->args()[0]) == retExpr);
 }
 
-}
